859cd3fa79d935645ce59d09c45a0e810a74a7cb
Sonie <garachu.muthoni@gmail.com>
2024-07-08 14:25:21 +0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import home from './Pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
+    return (
+      <Routes>
+        <Route exact path="/" element={<home />} />
+      </Routes>
   );
 }
 
